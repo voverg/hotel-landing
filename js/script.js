05e35209961cfd2c777444bc65399ebaad9ebcaa
voverg <voverg81@mail.ru>
2019-10-18 15:43:13 +0200

    Added galery section: slider

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Slick-slider settings
+$(document).ready(function(){
+  $('.sl').slick({
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    responsive: [
+        {
+        breakpoint: 960,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+            }
+        },
+        {
+        breakpoint: 720,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            arrows: false
+            }
+        }
+    ]
+  });
+});
